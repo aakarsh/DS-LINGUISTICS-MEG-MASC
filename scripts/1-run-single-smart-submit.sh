935ea5c6aa00597a10aaa541de63e27dee765b15
Aakarsh Nair <aakarsh.nair@gmail.com>
2025-10-12 21:13:15 +0200

    adding spacy download.

--- scripts/1-run-single-smart-submit.sh
+++ scripts/1-run-single-smart-submit.sh
@@ -4,7 +4,8 @@
 PROJECT_ROOT=/home/tu/tu_tu/tu_zxoxo45/TUE-SUMMER-2025/projects/DS-LINGUISTICS-MEG-MASC
 
 SUBJECT_IDS=(
-    "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11"
+    "01" "02" 
+    #"03" "04" "05" "06" "07" "08" "09" "10" "11"
 )
 
 # Check which subjects are already processed
@@ -47,7 +48,7 @@ cat > temp_remaining.sh << EOF
 #SBATCH --ntasks=64
 #SBATCH --output=slurm_logs/%A_%a.out
 #SBATCH --error=slurm_logs/%A_%a.err
-#SBATCH --partition=dev_cpu_il
+#SBATCH --partition=dev_cpu
 #SBATCH --time=00:29:00
 #SBATCH --array=${ARRAY_SPEC}%1
 
@@ -63,6 +64,7 @@ source ~/.bashrc
 source "\${PROJECT_ROOT}/.env.slurm"
 
 echo "Starting Slurm job \$SLURM_ARRAY_TASK_ID for subject: \$CURRENT_SUBJECT"
+uv run python -m spacy download en_core_web_sm
 
 uv run python \${PROJECT_ROOT}/scripts/1-run-single-subject.py --subject-id \$CURRENT_SUBJECT --max-workers 128
 EOF
